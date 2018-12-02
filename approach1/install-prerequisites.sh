333a76e50bd20e423cf3f80d43943496fa99896d
raspberrypisig <raspberrypisig@gmail.com>
2018-12-02 19:43:32 +1100

    Create install-prerequisites.sh

---
+++ approach1/install-prerequisites.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+sudo apt update
+
+# Prerequisites for opencv
+sudo apt install -y libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt install -y libxvidcore-dev libx264-dev
+sudo apt install -y qt4-dev-tools libatlas-base-dev python3-qt5
+
+# Alternative to OpenCV for capturing camera images
+sudo apt install -y fswebcam
+sudo apt install -y streamer
+
+# Install opencv
+sudo pip3 install --user opencv_contrib_python
+
+# Creating QR codes
+apt install -y qrencode
+
+
+
