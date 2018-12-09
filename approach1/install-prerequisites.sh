6b8dc51ce6e3bf84b676a0232e7568cdea5ff5ac
raspberrypisig <raspberrypisig@gmail.com>
2018-12-09 15:45:09 +1100

    Update install-prerequisites.sh

--- approach1/install-prerequisites.sh
+++ approach1/install-prerequisites.sh
@@ -6,20 +6,20 @@ sudo apt update
 sudo apt install -y libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
 sudo apt install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt install -y libxvidcore-dev libx264-dev
-sudo apt install -y qt4-dev-tools libatlas-base-dev python3-qt5
+sudo apt install -y qt4-dev-tools libatlas-base-dev python3-pyqt5
 
 # Alternative to OpenCV for capturing camera images
 sudo apt install -y fswebcam
 sudo apt install -y streamer
 
 # Install opencv
-sudo pip3 install --user opencv_contrib_python
+pip3 install --user opencv_contrib_python
 
 # Creating QR codes
-apt install -y qrencode
+sudo apt install -y qrencode
 
 # Install qrcode reader
-sudo pip3 install pyzbar[scripts]
+pip3 install pyzbar[scripts]
 
 
 
